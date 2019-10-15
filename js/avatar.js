80c35844f5bb4af366d639cd6cc34adabb1ee67f
Pavel Petrov <pavelpetrov7wc@gmail.com>
2019-10-15 22:05:30 +0700

    Сделал отображение загруженных фотографий

---
+++ js/avatar.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+
+  var avatarInputElement = document.querySelector('#avatar');
+  var avatarPreviewElement = document.querySelector('.ad-form-header__preview').querySelector('img');
+
+  avatarInputElement.addEventListener('change', function () {
+    var file = avatarInputElement.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = window.variables.FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatarPreviewElement.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+
+  });
+
+})();
